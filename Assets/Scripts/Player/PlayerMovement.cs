c5f18d0cd90a8cf7ff1db1a50a62bcb7d807e3c9
Stew Esho <StewardEsho@gmail.com>
2018-08-10 21:43:11 -0400

    physics based movement

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -2,26 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed = 2;
+    private Rigidbody2D rb;
 
     // Use this for initialization
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        Vector2 dir = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        Vector3 dir = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
         dir = speed * Time.deltaTime * Vector2.ClampMagnitude(dir, 1);
-        transform.Translate(dir);
+        rb.MovePosition(transform.position + dir);
     }
 
     void LateUpdate()
     {
-        transform.position.Set(Mathf.Round(transform.position.x / 16) * 16, Mathf.Round(transform.position.y / 16) * 16,
+        transform.position.Set(Mathf.Round(rb.position.x / 16) * 16, Mathf.Round(rb.position.y / 16) * 16,
             0);
     }
 }
