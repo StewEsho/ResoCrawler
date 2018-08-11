ac6696984560de03fe5f81a17688ccd8cafd121c
Stew Esho <StewardEsho@gmail.com>
2018-08-10 20:26:16 -0400

    Added Screen resizing script

---
+++ Assets/Scripts/Camera/ResizeScreen.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class ResizeScreen : MonoBehaviour
+{
+	private Camera cam;
+	private readonly int PPU = 16;
+	private int UNITS = 50;
+	private int RESOLUTION; 
+
+	void Awake()
+	{
+		Screen.SetResolution(800, 800, false);//defaults to 800 px
+	}
+	
+	void Start ()
+	{
+		RESOLUTION = UNITS * PPU;
+		cam = GetComponent<Camera>();
+		cam.orthographicSize = UNITS / 2.0f;
+		Screen.SetResolution(RESOLUTION, RESOLUTION, false);
+	}
+
+	public void SetUnits(int units)
+	{
+		UNITS = units;
+		cam.orthographicSize = UNITS / 2.0f;
+		Screen.SetResolution(RESOLUTION, RESOLUTION, false);
+		Debug.Log(string.Format("Resized the resolution to {0}", RESOLUTION));
+	}
+}
--- Assets/Scripts/DebugCode.cs
+++ Assets/Scripts/DebugCode.cs
@@ -5,28 +5,15 @@ using UnityEngine;
 
 public class DebugCode : MonoBehaviour
 {
-	private int PPU = 16;
-	private int SCREENSIZE = 800;
-	private int UNITS;
-	private Camera cam;
-
-	// Use this for initialization
 	void Start () {
-		Screen.SetResolution(SCREENSIZE, SCREENSIZE, false);
-		UNITS = (int) SCREENSIZE / PPU;
-		cam = Camera.main; //todo: make an actual script.
+		
 	}
 	
-	// Update is called once per frame
 	void Update () 
 	{
 		if (Input.GetKeyDown(KeyCode.G))
 		{
-			Debug.Log("Wow");
-			UNITS--;
-			SCREENSIZE = UNITS * PPU;
-			cam.orthographicSize = UNITS / 2.0f;
-			Screen.SetResolution(SCREENSIZE, SCREENSIZE, false);
+			Debug.Log("G key has been pressed.");
 		}
 		
 	}
